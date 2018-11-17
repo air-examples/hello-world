ab6ade7fa92638eda09cd15157bfc61d9f8c205e
Aofei Sheng <aofei@aofeisheng.com>
2018-11-17 22:16:34 +0800

    refactor: adapt to changes of modules

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/air-examples/hello-world
 
-require github.com/aofei/air v0.0.0-20181106102140-aa0c2b0aa1be
+require github.com/aofei/air v0.0.0-20181117141500-cc9ef405101b
--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import "github.com/aofei/air"
 
 func main() {
-	air.GET("/", func(req *air.Request, res *air.Response) error {
+	air.Default.GET("/", func(req *air.Request, res *air.Response) error {
 		return res.WriteString("Hello, 世界")
 	})
-	air.Serve()
+	air.Default.Serve()
 }
